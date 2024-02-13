7bc7d451fc70d8933ecda1240dacc29b5b159b4d
Sam <sameer1321khan@gmail.com>
2024-02-14 00:06:13 +0530

    added javascipt and some functionality created seekbar play pause buttons

---
+++ script.js
@@ -0,0 +1,101 @@
+console.log("Lets write JavaScript");
+let currentSong=new Audio();;
+
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
+
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:5500/song/");
+    let response = await a.text();
+    console.log(response);
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href.split("/song/")[1])
+        }
+        
+    }
+    return songs;
+}
+
+const playMusic = (track, pause=false)=>{
+    // let audio = new Audio("/song/" + track)
+    currentSong.src = "/song/" + track
+    if(!pause){
+        currentSong.play();
+        
+        play.src = "img/pause.svg"
+    }
+    document.querySelector(".songinfo").innerHTML = decodeURI(track);
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+}
+async function main() {
+    
+    // get the list of all the songs
+    let songs = await getSongs();
+    playMusic(songs[0],true)
+    // show all the song in the playlist
+    let songUL = document.querySelector(".songslist").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" src="img/music.svg" alt="">
+                            <div class="info">
+                                <div>${song.replaceAll("%20"," ")}</div>
+                                <div>Sameer</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="img/play.svg" alt="">
+                            </div>
+         </li>`;
+    }
+    // attach an Event listner to each song
+    Array.from(document.querySelector(".songslist").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click",element=>{
+            console.log(e.querySelector(".info").firstElementChild.innerHTML);
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+        })
+
+    })
+
+    // attach an event listner to play,next and previous
+    play.addEventListener("click", ()=>{
+        if(currentSong.paused){
+            currentSong.play()
+            play.src = "img/pause.svg"
+        }
+        else {
+            currentSong.pause()
+            play.src = "img/play.svg"
+        }
+    })
+    currentSong.addEventListener("timeupdate",(a)=>{
+        console.log(currentSong.currentTime, currentSong.duration);
+        document.querySelector(".songtime").innerHTML = `${
+            secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`
+        document.querySelector(".circle").style.left= (currentSong.currentTime/ currentSong.duration) * 100 + "%"
+    })
+
+    //add an event listner to seekbar
+    document.querySelector(".seekbar").addEventListener("click",e=>{
+        let percent = (e.offsetX/e.target.getBoundingClientRect().width)*100
+        document.querySelector(".circle").style.left= percent +"%";
+        currentSong.currentTime = (currentSong.duration *percent)/100
+    })
+}
+
+main();
+ 
